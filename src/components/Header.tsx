c638241abd30eefa5d6b24ea71b1ae8a4575d87e
sudhanshuraturi <sudhanshuraturi4@gmail.com>
2023-11-01 15:19:17 +0530

    dependency warning fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,6 +44,7 @@ const Header = () => {
     });
 
     return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleGptSearchClick = () => {
     // Toggle GPT Search
